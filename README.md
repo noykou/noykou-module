8f2d22db07d7121ecae54ac95d1cb1136c928888
SkyLineDev <51881159+SkyLineDev@users.noreply.github.com>
2019-07-19 13:46:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">NoyKou Module</h1>
+Discord => https://discord.gg/sMHk3H4
+Site => https://noykou.glitch.me
+
+<h5>Marche dès maintenant avec l'image du welcome !</h5>
---
+++ index.js
@@ -0,0 +1,10 @@
+
+const {get}= require("superagent");
+const baseURL = "https://noykou.glitch.me/api/v1/";
+
+module.exports.getImage = async function getImage(endpoint,options){
+return new Promise(async(resolve, reject) => {
+const {body} =await get(`${baseURL}${endpoint}?username=${options.username.split('').filter(a => ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "].includes(a.toLowerCase())).join("")}&url=${options.url}&text=${options.text}&message=${options.message}&back=${options.back}&style=${options.style}&roundcolor=${options.roundcolor}&textcolor=${options.textcolor}`)
+await resolve(body)
+})
+}
