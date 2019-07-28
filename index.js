2545fc8c4b3083b66473e28d91f4b8fdb470bd0a
Androz2091 <androz2091@gmail.com>
2019-07-28 19:42:13 +0200

    Update code style

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const { get } = require("superagent"),
+baseURL = "https://noykou.glitch.me/api/v1";
 
-const {get}= require("superagent");
-const baseURL = "https://noykou.glitch.me/api/v1/";
+module.exports = {
 
-module.exports.getImage = async function getImage(endpoint,options){
-return new Promise(async(resolve, reject) => {
-const {body} =await get(`${baseURL}${endpoint}?username=${options.username.split('').filter(a => ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "].includes(a.toLowerCase())).join("")}&url=${options.url}&action=${options.action}&text=${options.text}&message=${options.message}&back=${options.back}&style=${options.style}&roundcolor=${options.roundcolor}&textcolor=${options.textcolor}`)
-await resolve(body)
-})
+    /**
+     * Call an endpoints and returns the image
+     * @param {string} endpoint The endpoint to call
+     * @param {object} options The call options
+     * @returns {buffer}
+     */
+    async getImage(endpoint, options){
+        return new Promise(async(resolve, reject) => {
+            const { body } = await get(encodeURI(`${baseURL}/${endpoint}?username=${(options.username ? options.username.toLowerCase() : null)}&url=${options.url}&action=${options.action}&text=${options.text}&message=${options.message}&back=${options.back}&style=${options.style}&roundcolor=${options.roundcolor}&textcolor=${options.textcolor}`));
+            resolve(body);
+        });
+    }
+
 }
